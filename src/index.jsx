ca4c7d80e4797176a3dd113b294e719fc061e3aa
MaoScut <849345372@qq.com>
2017-08-28 11:57:24 +0800

    createStore的写法，不修饰，直接传入reducer和中间件，中间件其实是修饰dispatch的。

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from './reducers';
 import App from './router';
 
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 const render = (Component) => {
   ReactDom.render(
     <AppContainer>
-      <Component />
+      <Provider store={store}>
+        <Component />
+      </Provider>
     </AppContainer>,
     document.getElementById('root'),
   );
