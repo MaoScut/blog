e0b327baa902e8a7d1504c2bae73422269908186
MaoScut <849345372@qq.com>
2017-08-29 12:56:39 +0800

    明白了historyApiFallback的作用了，url返回404响应的时候，那么返回index

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,13 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    // publicPath: '/', //自动给静态资源路径加上前缀
   },
 
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: './dist',
+    // historyApiFallback: true, // 把404重定向到index，不能说是重定向，因为url没改变嘛，404会返回index
   },
   resolve: {
     extensions: ['.js', '.jsx'],
