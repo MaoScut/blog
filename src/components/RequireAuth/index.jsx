0484350e9b05109e7b5a8ad0c55e9963cf5fc684
MaoScut <849345372@qq.com>
2017-08-31 23:48:21 +0800

    检查是否已经登录，还没测试

---
+++ src/components/RequireAuth/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'redux';
+
+import history from '../../history';
+
+export default function (ComposedComponent) {
+  class Authentication extends React.Component {
+    // static contextTypes = {
+    //   router: React
+    // }
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        history.push('/login');
+      }
+    }
+
+    componentWillUpdate() {
+      if (!nextProps.authenticated) {
+        history.push('/login');
+      }
+    }
+    render() {
+      return <ComposedComponent {...this.props} />;
+    }
+  }
+  return connect(
+    state => ({
+      authenticated: state.auth.authenticated,
+    })(Authentication),
+  );
+}
