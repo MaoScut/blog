14d43276891cd8fe3156f7ef87be8412ad49c34e
MaoScut <849345372@qq.com>
2017-08-31 23:47:02 +0800

    登录注册的api

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 const STORAGE_KEY = 'BLOG';
+const REGIST_LOGIN = 'REGIST_LOGIN';
+
 export function fetchArticles() {
   return new Promise((resolve) => {
     // localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
@@ -11,4 +13,33 @@ export function fetchArticles() {
 }
 export function receiveArticles() {
   
+}
+
+export function registerUser({ email, password }, callback) {
+  const json = window.localStorage.getItem(REGIST_LOGIN);
+  const users = JSON.parse(json) || [];
+
+  if (users.find(user => user.email === email) !== undefined) {
+    callback({
+      message: 'this email has been registed!',
+    });
+  } else {
+    window.localStorage.setItem(REGIST_LOGIN, JSON.stringify(users.concat({
+      email,
+      password,
+    })));
+    callback(null, email);
+  }
+}
+
+export function loginUser({ email, password }, callback) {
+  const json = window.localStorage.getItem(REGIST_LOGIN);
+  const users = JSON.parse(json) || [];
+  if (users.findIndex(user => user.email === email && user.password === password) === -1) {
+    callback({
+      message: 'email and password not existed!',
+    });
+  } else {
+    callback(null, email);
+  }
 }
