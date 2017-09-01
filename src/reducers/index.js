870c8f76c982c3d0465cc39d9af80a0917703f58
MaoScut <849345372@qq.com>
2017-09-01 13:13:40 +0800

    导入auth，error 的reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 import articles from './articles';
 import article from './article';
+import auth from './auth';
+import error from './error';
 
 const root = combineReducers({
   articles,
   article,
+  auth,
+  error,
 });
 export default root;
