ec256037c306167aab756dd680a2938089f69d58
MaoScut <849345372@qq.com>
2017-09-01 13:12:47 +0800

    根据是否登录来决定显示内容

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 require('./main.scss');
 // require('./ionicons.css');
 
-export default function () {
+export default function ({ auth, actions }) {
+  if (auth.auth) {
+    return (
+      <div className="header">
+        <img src="/images/logo.png" alt="logo" />
+        <div className="right"><button onClick={() => actions.logoutUser()}>logout</button></div>
+        <div className="right"><Link to="/home">{auth.name}</Link></div>
+      </div>
+    );
+  }
   return (
     <div className="header">
       <img src="/images/logo.png" alt="logo" />
       {/* <div className="search" /> */}
-      <div className="right"><button>signup</button></div>
-      <div className="right"><button>login</button></div>
+      <div className="right"><button><Link to="/home">home</Link></button></div>      
+      <div className="right"><button><Link to="/regist">signup</Link></button></div>
+      <div className="right"><button><Link to="/login">login</Link></button></div>
       {/* <div><button>search</button></div>
       <div><button>more</button></div> */}
     </div>
