5d778e01ddd11d43ac437e023f3f31b533c8b19f
MaoScut <849345372@qq.com>
2017-09-03 00:12:42 +0800

    读写json

--- data/dataAccess.js
+++ data/dataAccess.js
@@ -1,15 +1,59 @@
-let fs = require('fs');
-let path = require('path');
+const fs = require('fs');
+const path = require('path');
+
 function fetchAll(func) {
-  const p = path.join(__dirname, 'database.json')
+  const p = path.join(__dirname, 'article.json');
   fs.readFile(p, 'utf8', (err, data) => {
     if (err) {
       console.log(err);
       return;
-    };
+    }
     // 只是返回字符串而已，转化为数组自己去搞
     func(data);
-  })
+  });
+}
+function readAndWrite() {
+  // const p = path.join(__dirname, 'article.json');  
+  // fs.open(p, 'rw', (err, fd) => {
+  //   if (err) {
+  //     console.log(err);
+  //     return;
+  //   }
+  //   fs.read(fd, 'utf8', (err, data) => {
+  //     if (err) {
+  //       console.log(err);
+  //       // return;
+  //     }
+  //     const orginArr = JSON.parse(data);
+  //     fs.write(fd,)
+  //   })
+  // })
+  const p = path.join(__dirname, 'article.json');
+  fs.readFile(p, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    // 只是返回字符串而已，转化为数组自己去搞
+    const originArr = JSON.parse(data);
+    const newData = JSON.stringify(originArr.concat({
+      articleId: '4444442',
+      title: 'mytitle2',
+      content: 'content2',
+      ownerId: '4444432',
+      articleType: 'nature',
+    }));
+    fs.writeFile(p, newData, (e) => {
+      if (e) console.log(e);
+      fs.readFile(p, 'utf8', (er, d) => {
+        if (e) {
+          console.log(e);
+        }
+        console.log(JSON.parse(d).length);
+      });
+    });
+  });
 }
 console.log('hhh');
-fetchAll((data) => console.log(JSON.parse(data)));
+// fetchAll((data) => console.log(JSON.parse(data)));
+readAndWrite();
