e388bb06a95624e4aeab15fad1387dd90899ecba
MaoScut <849345372@qq.com>
2017-09-02 11:36:18 +0800

    增加文章的入口

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 import List from '../List';
 // import * as actions from '../../action/async';
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.add = this.add.bind(this);
+  }
   componentDidMount() {
     this.props.actions.fetchArticles();
   }
+  add() {
+    this.props.actions.add({
+      title: 'aa',
+      content: 'bb',
+      articleType: 'cc',
+    });
+  }
   render() {
-    return <List articles={this.props.articles} />;
+    return (
+      <div>
+        <List articles={this.props.articles} />
+        <button onClick={this.add}>new article</button>
+        <Link to="/myarticle" >new article </Link>
+      </div>
+    );
   }
 }
 //  connect(
