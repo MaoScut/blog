d05aa27de0b7582e273b92c9bddc3eddc7572fa7
MaoScut <849345372@qq.com>
2017-09-03 10:42:38 +0800

    多余的reducer

--- src/reducers/article.js
+++
@@ -1,9 +0,0 @@
-import { handleActions } from 'redux-actions';
-import * as ActionTypes from '../actionTypes';
-
-const initialState = null;
-export default handleActions({
-  [ActionTypes.FETCH_EXACT_ARTICLES](state, action) {
-    return action.payload;
-  },
-}, initialState);
