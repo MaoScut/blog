57ce9a70d451917275453cea0fc9e0c68c9c52b3
MaoScut <849345372@qq.com>
2017-09-05 20:36:35 +0800

    服务器通信api

---
+++ src/store/ajax.js
@@ -0,0 +1,42 @@
+function ajax(method, url, data) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onreadystatechange = function h() {
+      if (xhr.status === 200 && xhr.readyState === 4) {
+        resolve(xhr.responseText);
+      } else if (xhr.status >= 400) reject(xhr.responseText);
+    };
+    xhr.setRequestHeader('content-type', 'application/json');
+    if (data) xhr.send(JSON.stringify(data));
+    else xhr.send();
+  });
+}
+export function fetchArticles() {
+  return ajax('get', '/fetchAll').then(data => JSON.parse(data));
+}
+
+export function registerUser({ email, password }) {
+  return ajax('post', '/regist', { email, password });
+}
+
+export function loginUser({ email, password }) {
+  return ajax('post', '/login', { email, password });
+}
+
+export function save(article) {
+  return ajax('post', '/save', article);
+}
+
+export function fetchPrivateArticles() {
+  return ajax('get', '/fetchPrivate').then(data => JSON.parse(data));
+  // return fetchArticles().then(result => result.filter(v => v.ownerId === id));
+}
+
+export function add() {
+
+}
+export function deleteArticle(id) {
+  return ajax('post', '/deleteArticle', { articleId: id });
+}
+
