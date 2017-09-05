85d21f3933b35a2ff71cd49b05de78cc094d48c6
MaoScut <849345372@qq.com>
2017-09-05 20:36:00 +0800

    自己配置server，一大堆中间件，history api fallback还没搞定。

--- server/index.js
+++ server/index.js
@@ -1,72 +1,98 @@
-let http = require('http');
-let path = require('path');
-let session = require('express-session');
-let Express = require('express');
-let bodyParser = require('body-parser');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const config = require('../webpack.config.selfserver');
+const path = require('path');
+const Express = require('express');
+const FileStore = require('session-file-store')(session);
+// const history = require('connect-history-api-fallback');
+// const RedisStore = require('connect-redis')(session);
+const serveStatic = require('serve-static');
+const database = require('./data');
+// const sessionStore = require('./server/sessionStore').store;
 
-let app = Express();
-// parse application/x-www-form-urlencoded 
-app.use(bodyParser.urlencoded({ extended: false }))
+const app = new Express();
+const port = 8080;
 
-// parse application/json 
-app.use(bodyParser.json())
-app.set('views', __dirname + 'views');
-// Use the session middleware 
-// app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
-}));
-
-// Access the session as req.session 
-app.get('/', function(req, res, next) {
- if (req.session.views) {
-   req.session.views++
-   res.setHeader('Content-Type', 'text/html')
-   res.write('<p>views: ' + req.session.views + '</p>')
-   res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
-   res.end()
- } else {
-   req.session.views = 1
-   res.end('welcome to the session demo. refresh!')
- }
-})
-
-// app.get('/', (req, res) => {
-//   // res.render('Home.html');
-//   res.sendFile(path.resolve('./views/Home.html'), (err) => {
-//     if(err) {
-//       console.log(err);
-//       res.status(err.status).end();
-//     } else {
-//       console.log('send home.html');
-//     }
-//   })
+const compiler = webpack(config);
+// app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+// app.use(webpackHotMiddleware(compiler));
+app.use(serveStatic(path.resolve('./dist')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use((req, res, next) => {
+//   // next();
+//   if (res.status === 404) console.log('333');
+//   else next();
 // });
+// app.use(history());
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  // contentBase: './dist',
+}));
+app.use(webpackHotMiddleware(compiler));
 
-app.get('/login', (req, res) => {
-  // res.render('Home.html');
-  res.sendFile(path.resolve('./views/Login.html'), (err) => {
-    if(err) {
-      console.log(err);
-      res.status(err.status).end();
-    } else {
-      console.log('send login.html');
-    }
-  })
+app.use(session({
+  store: new FileStore(),
+  // store: new RedisStore(),
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: false,
+  cookie: { maxAge: 60000 },
+}));
+app.use((req, res, next) => {
+  // console.log('sid: ' + req.sessionID);
+  // console.log(req.session);
+  next();
+});
+
+app.get('/fetchAll', (req, res) => {
+  database.getAllArticles().then(data => res.end(data));
+});
+app.get('/fetchPrivate', (req, res) => {
+  database.fetchPrivateArticles(req.session.userId).then(data => res.end(data));
 });
 app.post('/login', (req, res) => {
-  let data = JSON.stringify(req.body);
-  console.log(data);
-  res.sendFile(path.resolve('./views/Login.html'), (err) => {
-    if(err) {
-      console.log(err);
-      res.status(err.status).end();
-    } else {
-      console.log('send login.html');
-    }
-  })
-})
-http.createServer(app).listen(3000, () => console.log('listening on port 3000!'));
+  database.loginCheck(req.body.email, req.body.password).then((acc) => {
+    req.session.userId = acc.id;
+    req.session.email = acc.email;
+    req.session.save((err) => {
+      if (err) console.log(err);
+      else console.log('save session');
+    });
+    res.setHeader('Set-Cookie', [`sid=${req.sessionID}`]);
+    res.end();
+  });
+});
+app.post('/regist', (req, res) => {
+  database.registerUser(req.body.email, req.body.password).then((acc) => {
+    req.session.email = acc.email;
+    req.session.userId = acc.id;
+    req.session.save((err) => {
+      if (err) console.log(err);
+      else console.log('save session');
+    });
+    res.setHeader('Set-Cookie', [`token=${req.sessionID}`, [`email=${acc.email}`]]);
+    res.end();
+  });
+});
+app.post('/save', (req, res) => {
+  req.body.ownerId = req.session.userId;
+  database.save(req.body).then(() => res.end());
+});
+app.post('/deleteArticle', (req, res) => {
+  database.deleteArticle(req.body.articleId).then(() => res.end());
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+  }
+});
+
