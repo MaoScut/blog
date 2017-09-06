0121d394a0e1d192eed742b718dfbbfe17d374e3
MaoScut <849345372@qq.com>
2017-09-06 11:36:08 +0800

    删除多余组件引用

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
-// import Home from '../components/Home';
 import Detail from '../components/Detail';
 import Editor from '../components/Editor';
-// import Header from '../components/Header';
-// import Login from '../components/Login';
-// import Regist from '../components/Regist';
-import { Home, Header, Error, PrivateArticles } from '../components/Connected';
+import { Home, Header, Error, PrivateArticles, Login } from '../components/Connected';
+import RequireAuth from '../components/RequireAuth';
 import history from '../history';
 
 export default () => (
@@ -15,83 +12,10 @@ export default () => (
       <Route path="/" component={Header} />
       <Route path="/" component={Error} />
       <Route exact path="/" component={Home} />
-      <Route exact path="/home" component={Home} />
+      <Route exact path="/login" component={Login} />
       <Route path="/detail/:articleId" component={Detail} />
       <Route path="/editor/:articleId" component={Editor} />
-      <Route exact path="/private" component={PrivateArticles} />
+      <Route exact path="/private" component={RequireAuth(PrivateArticles)} />
     </div>
   </Router>
 );
-// import React from 'react'
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link,
-// } from 'react-router-dom'
-
-// const ParamsExample = () => (
-//   <Router>
-//     <div>
-//       <h2>Accounts</h2>
-//       <ul>
-//         <li><Link to="/netflix">Netflix</Link></li>
-//         <li><Link to="/zillow-group">Zillow Group</Link></li>
-//         <li><Link to="/yahoo">Yahoo</Link></li>
-//         <li><Link to="/modus-create">Modus Create</Link></li>
-//       </ul>
-
-//       <Route path="/:id" component={Child}/>
-//     </div>
-//   </Router>
-// )
-
-// const Child = ({ match }) => (
-//   <div>
-//     <h3>ID: {match.params.id}</h3>
-//   </div>
-// )
-
-// export default ParamsExample
-// import React from 'react'
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link
-// } from 'react-router-dom'
-
-// const PEEPS = [
-//   { id: 0, name: 'Michelle', friends: [ 1, 2, 3 ] },
-//   { id: 1, name: 'Sean', friends: [ 0, 3 ] },
-//   { id: 2, name: 'Kim', friends: [ 0, 1, 3 ], },
-//   { id: 3, name: 'David', friends: [ 1, 2 ] }
-// ]
-
-// const find = (id) => PEEPS.find(p => p.id == id)
-
-// const RecursiveExample = () => (
-//   <Router>
-//     <Person match={{ params: { id: 0 }, url: '' }}/>
-//   </Router>
-// )
-
-// const Person = ({ match }) => {
-//   const person = find(match.params.id)
-
-//   return (
-//     <div>
-//       <h3>{person.name}’s Friends</h3>
-//       <ul>
-//         {person.friends.map(id => (
-//           <li key={id}>
-//             <Link to={`${match.url}/${id}`}>
-//               {find(id).name}
-//             </Link>
-//           </li>
-//         ))}
-//       </ul>
-//       <Route path={`${match.url}/:id`} component={Person}/>
-//     </div>
-//   )
-// }
-
-// export default RecursiveExample
